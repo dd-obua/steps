3f9edbd2badc1beabf5a0d1e7c8b5e9bdc973d61
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-10 18:05:24 +0300

    Render display based on x button click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,7 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 const App = () => {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const handleClick = (event) => {
     const { name } = event.target;
@@ -12,38 +13,45 @@ const App = () => {
   };
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? 'active' : ''}>1</div>
-        <div className={step >= 2 ? 'active' : ''}>2</div>
-        <div className={step >= 3 ? 'active' : ''}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          name="prev"
-          style={{
-            pointerEvents: step <= 1 ? 'none' : 'auto',
-            opacity: step <= 1 ? 0.5 : 1,
-          }}
-          onClick={handleClick}
-        >
-          Previous
-        </button>
-        <button
-          name="next"
-          style={{
-            pointerEvents: step >= 3 ? 'none' : 'auto',
-            opacity: step >= 3 ? 0.5 : 1,
-          }}
-          onClick={handleClick}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? 'active' : ''}>1</div>
+            <div className={step >= 2 ? 'active' : ''}>2</div>
+            <div className={step >= 3 ? 'active' : ''}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              name="prev"
+              style={{
+                pointerEvents: step <= 1 ? 'none' : 'auto',
+                opacity: step <= 1 ? 0.5 : 1,
+              }}
+              onClick={handleClick}
+            >
+              Previous
+            </button>
+            <button
+              name="next"
+              style={{
+                pointerEvents: step >= 3 ? 'none' : 'auto',
+                opacity: step >= 3 ? 0.5 : 1,
+              }}
+              onClick={handleClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
