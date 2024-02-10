f9578f1ac99b5c319ad4ea3a15d1bc29f9038fd2
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-10 15:50:23 +0300

    Craft the interface manually

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,24 @@
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
+
 const App = () => {
-  return <p>Hello React</p>;
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ background: '#7950f2', color: '#fff' }}>Previous</button>
+        <button style={{ background: '#7950f2', color: '#fff' }}>Next</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
