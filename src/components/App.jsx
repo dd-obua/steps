6c1324da1e2118329381b67142e70980fecb4576
Obua Denis Daniel <dd.obua@gmail.com>
2024-02-10 17:46:40 +0300

    Let the application respond to button click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,47 @@
+import { useState } from 'react';
+
 const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
 const App = () => {
-  const step = 1;
+  const [step, setStep] = useState(1);
+
+  const handleClick = (event) => {
+    const { name } = event.target;
+    if (name === 'prev') step > 1 && setStep(step - 1);
+    if (name === 'next') step < 3 && setStep(step + 1);
+  };
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={step >= 1 ? 'active' : ''}>1</div>
+        <div className={step >= 2 ? 'active' : ''}>2</div>
+        <div className={step >= 3 ? 'active' : ''}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ background: '#7950f2', color: '#fff' }}>Previous</button>
-        <button style={{ background: '#7950f2', color: '#fff' }}>Next</button>
+        <button
+          name="prev"
+          style={{
+            pointerEvents: step <= 1 ? 'none' : 'auto',
+            opacity: step <= 1 ? 0.5 : 1,
+          }}
+          onClick={handleClick}
+        >
+          Previous
+        </button>
+        <button
+          name="next"
+          style={{
+            pointerEvents: step >= 3 ? 'none' : 'auto',
+            opacity: step >= 3 ? 0.5 : 1,
+          }}
+          onClick={handleClick}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
